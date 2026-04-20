284e3094c2627129485ec195cbafc013b77f2145
Voxmic <farelsurya16@gmail.com>
2026-04-20 14:27:57 +0700

    Mengubah harga dan stok

--- Week2/Main.java
+++ Week2/Main.java
@@ -4,14 +4,15 @@ import Week2.base.Produk;
 
 public class Main {
     public static void main(String[] args) {
-        Week2.base.Produk p = new Produk("Indomie Goreng",3500,100, "Makanan");
+        Produk p = new Produk("Indomie Goreng",5000,100,"Makanan");
 
-        // System.out.println(p.nama);
-        // System.out.println(p.harga);
-        // System.out.println(p.stok);
-        // System.out.println(p.kategori);
-        
-        p.tampilkanInfo();
+        System.out.println(p.getNama());
+        System.out.println(p.getHarga());;
+        p.setHarga(10000);
+        System.out.println(p.getStok());
+        p.setStok(50);
+        System.out.println(p.getNama());
+        System.out.println(p.getHarga());
+        System.out.println(p.getStok());
     }
-    
 }
