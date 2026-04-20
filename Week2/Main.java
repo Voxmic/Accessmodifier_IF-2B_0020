906e5cb5fd9a80d1c866f9efe69df97076b3d23b
Voxmic <farelsurya16@gmail.com>
2026-04-20 12:28:30 +0700

    first commit

---
+++ Week2/Main.java
@@ -0,0 +1,17 @@
+package Week2;
+
+import Week2.base.Produk;
+
+public class Main {
+    public static void main(String[] args) {
+        Week2.base.Produk p = new Produk("Indomie Goreng",3500,100, "Makanan");
+
+        // System.out.println(p.nama);
+        // System.out.println(p.harga);
+        // System.out.println(p.stok);
+        // System.out.println(p.kategori);
+        
+        tampilkanInfo(p);
+    }
+    
+}
---
+++ Week2/base/Produk.java
@@ -0,0 +1,23 @@
+package Week2.base;
+
+public class Produk {
+    private String nama;
+    String kategori;
+    protected double harga;
+    public int stok;
+
+    public Produk(String nama, double harga, int stok, String kategori) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+        this.kategori = kategori;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Produk: " + nama);
+        System.out.println("Harga: Rp. " + harga);
+        System.out.println("Stok: " + stok);
+        System.out.println("Kategori: " + kategori);
+        System.out.println("-------------------------");
+    }
+}
